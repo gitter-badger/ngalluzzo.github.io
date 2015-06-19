62c004fd61d1a242b1f375e2f6e4fb7ac790240b
ngalluzzo <nicolas@knackhq.com>
2015-06-19 09:27:04 -0700

    init
    push

--- nga.js
+++ nga.js
@@ -1,10 +1,12 @@
-	window.KnackInit = function($){
-		if (!Knack.session.user) {
-	  Knack.user.save({
-	    "email": "nicolas@knackhq.com",
-	    "password": "nic"
-	  }, {
-	    success: function () {},
-	    error: function () {}
-	  });
-	}}
+window.KnackInit = function($) { 
+	if (!Knack.session.user) {
+  Knack.user.save({
+         email: "nicolas@knackhq.com",
+         password: "nic",
+  }, {
+    success: function () {},
+    error: function () {}
+  });
+}
+
+}
