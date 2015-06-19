8e702558fcf990473f3e2064c44dddbdb48d38ce
ngalluzzo <nicolas@knackhq.com>
2015-06-19 09:23:04 -0700

    push
    knack

--- nga.js
+++ nga.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-	var auth_headers = {
-		'X-Knack-Application-ID': ''
-	  , 'X-Knack-REST-API-KEY': ''
-	};
-
-	var kn_url = 'https://api.knackhq.com/v1/objects/';
-
-});
+	if (!Knack.session.user) {
+  Knack.user.save({
+    "email": "nicolas@knackhq.com",
+    "password": "nic"
+  }, {
+    success: function () {},
+    error: function () {}
+  });
+}
